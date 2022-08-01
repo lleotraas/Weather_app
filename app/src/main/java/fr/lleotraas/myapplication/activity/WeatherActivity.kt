27fd893e96636b798065a33ad215630320dcdb28
Charles Freddy <lleotraas@gmail.com>
2022-08-01 10:07:55 +0200

    Feature: Add back arrow in WeatherActivity.kt

--- app/src/main/java/fr/lleotraas/myapplication/activity/WeatherActivity.kt
+++ app/src/main/java/fr/lleotraas/myapplication/activity/WeatherActivity.kt
@@ -2,6 +2,8 @@ package fr.lleotraas.myapplication.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import fr.lleotraas.myapplication.R
 import fr.lleotraas.myapplication.databinding.ActivityWeatherBinding
 
 class WeatherActivity : AppCompatActivity() {
@@ -12,5 +14,19 @@ class WeatherActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityWeatherBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        configureToolbar()
+    }
+
+    private fun configureToolbar() {
+        setSupportActionBar(binding.activityWeatherToolbar.toolbar)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        supportActionBar!!.setDisplayShowHomeEnabled(true)
+        val backArrow = ContextCompat.getDrawable(this, R.drawable.ic_arrow_back)
+        supportActionBar!!.setHomeAsUpIndicator(backArrow)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        finish()
+        return true
     }
 }
--- app/src/main/java/fr/lleotraas/myapplication/fragment/WeatherFragment.kt
+++ app/src/main/java/fr/lleotraas/myapplication/fragment/WeatherFragment.kt
@@ -7,9 +7,7 @@ import android.content.Intent
 import android.content.IntentFilter
 import android.os.Build
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.DividerItemDecoration
